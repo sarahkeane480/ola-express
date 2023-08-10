002f12da041a2303d283d5b3c0b54b6cacdbe315
Replit user <>
2023-08-10 15:32:44 +0000

    did stuff

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  let title = "My Home Page"
+  res.render('index', { "title": title })
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page!!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
